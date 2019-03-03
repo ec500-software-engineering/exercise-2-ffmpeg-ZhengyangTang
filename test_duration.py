d07a08584e00dcbbe9591c85c4bee1952fce3a57
ZhengyangTang <43021026+ZhengyangTang@users.noreply.github.com>
2019-03-03 18:15:43 -0500

    Add files via upload

---
+++ test_duration.py
@@ -0,0 +1,25 @@
+from pytest import approx
+import subprocess
+import json
+
+def ffprobe_sync(file) -> dict:
+    """ get media metadata """
+    meta = subprocess.check_output(['ffprobe', '-v', 'warning',
+                                    '-print_format', 'json',
+                                    '-show_streams',
+                                    '-show_format',
+                                    str(file)],universal_newlines=True)
+    return json.loads(meta)
+
+def test_duration():
+    fnin = "C:/Users/96171/PycharmProjects/exercise2_ec500/test_video.mp4"
+    fnout = "C:/Users/96171/PycharmProjects/exercise2_ec500/test_video.mp4_480.mp4"
+
+    orig_meta = ffprobe_sync(fnin)
+    orig_duration = float(orig_meta['streams'][0]['duration'])
+
+
+    meta_480 = ffprobe_sync(fnout)
+    duration_480 = float(meta_480['streams'][0]['duration'])
+
+    assert round(orig_duration) == approx(round(duration_480))
