c9b60e82339433e86b0adcffd87625f37c2f3917
ZhengyangTang <43021026+ZhengyangTang@users.noreply.github.com>
2019-03-06 15:42:01 -0500

    Add files via upload

--- test_duration.py
+++ test_duration.py
@@ -1,21 +1,29 @@
+from pytest import approx
 import subprocess
 import json
 from pathlib import Path
-from pytest import approx
 
-def ffprobe_tang(file: Path) -> dict:
-    meta1 = subprocess.check_output(['ffprobe111', '-v', 'warning', '-print_format',
-                                         'json', '-show_streams', '-show_format', file],
-                                        universal_newlines = True)
-    return json.loads(meta1)
+
+
+def ffprobe(file: Path) -> dict:
+    meta = subprocess.check_output(['ffprobe', '-v', 'warning',
+                                    '-print_format', 'json',
+                                    '-show_streams',
+                                    '-show_format',
+                                    str(file)],universal_newlines=True)
+    return json.loads(meta)
 
 def test_duration():
+    #fnin = "C:/Users/96171/PycharmProjects/exercise2_ec500/test_video.mp4"
+    #fnout = "C:/Users/96171/PycharmProjects/exercise2_ec500/test_video.mp4_480.mp4"
+    fnin = "test_video.mp4"
+    fnout = "test_video.mp4_480.mp4"
 
-    original_meta = ffprobe_tang('test_video.mp4')
-    t480_meta = ffprobe_tang('test_video.mp4_480.mp4')
+    original_meta = ffprobe(fnin)
+    orig_duration = float(original_meta['streams'][0]['duration'])
 
-    duration = float(original_meta['streams'][0]['duration'])
-    t480_duration = float(t480_meta['streams'][0]['duration'])
-   
-    assert duration == approx(t480_duration)
+    meta_480 = ffprobe(fnout)
+    duration_480 = float(meta_480['streams'][0]['duration'])
+
+    assert round(orig_duration) == approx(round(duration_480))
 
