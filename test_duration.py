278273d5490a78e3dfd0515066d986f1e6bbefc8
ZhengyangTang <43021026+ZhengyangTang@users.noreply.github.com>
2019-03-06 15:22:07 -0500

    Update test_duration.py

--- test_duration.py
+++ test_duration.py
@@ -1,26 +1,21 @@
-from pytest import approx
 import subprocess
 import json
 from pathlib import Path
+from pytest import approx
 
-
-def ffprobe(file:Path) -> dict:
-    meta = subprocess.check_output(['ffprobe', '-v', 'warning',
-                                    '-print_format', 'json',
-                                    '-show_streams',
-                                    '-show_format',
-                                    file],universal_newlines=True)
-    return json.loads(meta)
+def ffprobe(file: Path) -> dict:
+    meta_json = subprocess.check_output(['ffprobe', '-v', 'warning', '-print_format',
+                                         'json', '-show_streams', '-show_format', file],
+                                        universal_newlines = True)
+    return json.loads(meta_json)
 
 def test_duration():
-    fnin = "test_video.mp4"
-    fnout = "test_video.mp4_480.mp4"
 
-    orig_meta = ffprobe(fnin)
-    orig_duration = float(orig_meta['streams'][0]['duration'])
-
-
-    meta_480 = ffprobe(fnout)
-    duration_480 = float(meta_480['streams'][0]['duration'])
-
-    assert round(orig_duration) == approx(round(duration_480))
+    orig_meta = ffprobe('test_video.mp4')
+    meta_480 = ffprobe('test_video.mp4_480.mp4')
+    #meta_720 = ffprobe('transcoded_video_720p.mp4')
+    video_duration = float(orig_meta['streams'][0]['duration'])
+    video_480_duration = float(meta_480['streams'][0]['duration'])
+    #video_720_duration = float(meta_720['streams'][0]['duration'])
+    assert video_duration == approx(video_480_duration)
+    #assert video_duration == approx(video_720_duration)
