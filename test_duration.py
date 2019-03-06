006ae2aa9dbc1ea67e412fc34f406297e9c10f52
ZhengyangTang <43021026+ZhengyangTang@users.noreply.github.com>
2019-03-06 15:39:20 -0500

    Update test_duration.py

--- test_duration.py
+++ test_duration.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from pytest import approx
 
 def ffprobe_tang(file: Path) -> dict:
-    meta1 = subprocess.check_output(['ffprobe', '-v', 'warning', '-print_format',
+    meta1 = subprocess.check_output(['ffprobe111', '-v', 'warning', '-print_format',
                                          'json', '-show_streams', '-show_format', file],
                                         universal_newlines = True)
     return json.loads(meta1)
